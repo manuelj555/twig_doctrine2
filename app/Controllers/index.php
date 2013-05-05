50f5c24f197e9a876df00c2b5d599dfebde5afa4
manuel_j555 <programador.manuel@gmail.com>
2013-05-05 19:21:22 -0430

    agregando funcionalidad para el agregado de articulos con cantidad

--- app/Controllers/index.php
+++ app/Controllers/index.php
@@ -8,6 +8,13 @@ $articulos = App::doctrine()
         ->createQuery('SELECT a.nombre value, UPPER(a.nombre) label, a FROM Articulo a')
         ->getArrayResult();
 
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    var_dump($_POST);
+    foreach($_POST['art'] as $i => $e){
+        var_dump($i, $e);
+    }
+}
+
 App::twig()->display('factura/index.twig', array(
     'personas' => $personas,
     'articulos' => $articulos,
