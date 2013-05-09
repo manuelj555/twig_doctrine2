955c50f273d056ea500e50d9706b636bdff3baff
Manuel Aguirre <programador.manuel@gmail.com>
2013-05-09 11:22:36 -0430

    usando el diseño del foundation.zurb

---
+++ tabla.php
@@ -0,0 +1,23 @@
+<style>
+    .mi_tabla tr td{border-bottom: 1px solid #000}
+    .mi_tabla tr:last-child td{border-bottom: none}
+</style>
+
+<table class="mi_tabla" style="width: 400px">
+    <tr><td>1</td></tr>
+    <tr><td>1</td></tr>
+    <tr><td>1</td></tr>
+    <tr><td>1</td></tr>
+    <tr><td>2</td></tr>
+    <tr><td>3</td></tr>
+    <tr><td>4</td></tr>
+</table>
+<table class="mi_tabla2" style="width: 400px">
+    <tr><td>1</td></tr>
+    <tr><td>1</td></tr>
+    <tr><td>1</td></tr>
+    <tr><td>1</td></tr>
+    <tr><td>2</td></tr>
+    <tr><td>3</td></tr>
+    <tr><td>4</td></tr>
+</table>
